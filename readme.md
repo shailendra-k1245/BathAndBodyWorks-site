db831df73ee006ba7c0ad689fe9af1f23f2bddfe
Anup <haldaranup746@gmail.com>
2022-05-04 20:23:13 +0530

    add footer section

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-# BathAndBodyWorks
+
+# Bodybathworks
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Footer from "./components/footer/Footer";
+import "./global.scss"
+import "./App.scss"
+
+export default function App() {
+  return <div>
+     <Footer />
+  </div>;
+}

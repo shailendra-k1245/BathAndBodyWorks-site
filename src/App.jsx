425ec946221c34410ae883cf5b64297bd3e30472
Anup <haldaranup746@gmail.com>
2022-05-05 19:53:22 +0530

    create soap section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Footer from "./components/footer/Footer";
 import "./global.scss"
 import "./App.scss"
+import Soap from "./components/soaps/Soap";
 
 export default function App() {
   return <div>
+    <Soap />
      <Footer />
+
   </div>;
 }
---
+++ src/components/soaps/Soap.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Midsoap from './Midsoap'
+import "./soap.scss"
+
+export default function Soap() {
+  return (
+    <div className='soapMain'>
+        <div className='soapHead'>
+            <p><a href="google.com">HOME</a> / HAND SOAPS & SANITIZERS</p>
+        </div>
+
+        <Midsoap />
+
+    </div>
+  )
+}

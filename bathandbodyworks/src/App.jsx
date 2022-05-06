f9277a5451df1634528526bbc7187b2f58aeb02d
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-05-06 22:55:12 +0530

    newnow page done

---
+++ bathandbodyworks/src/App.jsx
@@ -0,0 +1,14 @@
+
+// import './App.css';
+
+import { NewNow } from "./components/NewNow";
+
+function App() {
+  return (
+    <div className="App">
+      <NewNow/>
+    </div>
+  );
+}
+
+export default App;
